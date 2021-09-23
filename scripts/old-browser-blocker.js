290d5a3a1cb58b6b126f8983c7a7aecf2abd4daf
electromeow <gokgoz.berat123@gmail.com>
2021-09-23 12:13:16 +0300

    Move static and scripts folders into src folder

--- scripts/old-browser-blocker.js
+++
@@ -1,6 +0,0 @@
-try {
-  window.fetch("/");
-} catch (e) {
-  document.body.innerHTML =
-    "<h1>Görünüşe göre Internet Explorer veya başka bir eski kalmış tarayıcı kullanıyorsunuz.<br>Lütfen sadece bu site değil, kullanacağınız pek çok sitenin düzgün çalışması için modern bir tarayıcının(Firefox veya Chromium-tabanlı olan Chrome, Opera, Brave gibi bir tarayıcı tavsiye edilir) yeni sürümlerine geçiş yapınız.</h1>";
-}
--- scripts/toggleMenu.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-  document.querySelector("nav ul").classList.toggle("opened");
-  const hamburgerIcon = document.querySelector("i[name=hamburger]");
-  hamburgerIcon.classList.toggle("fa-bars");
-  hamburgerIcon.classList.toggle("fa-times");
-}
